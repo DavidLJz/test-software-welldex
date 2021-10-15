5c67b2781fb425d5985f9292ca7a586213202471
DavidLJz <4dlj1995@gmail.com>
2021-10-15 09:50:30 -0500

    Agrega metodos actualizar fecha arribo/zarpe

--- src/Operations/Export.php
+++ src/Operations/Export.php
@@ -26,4 +26,25 @@ class Export extends Operation
 		$this->fecha_zarpe = $time;
 		$this->pais_destino = $pais;
 	}
+
+	public function actualizarFechaZarpe($time) :self
+	{
+		if ( !$time instanceof \DateTime ) {
+			if ( empty((int) $time) ) {
+				throw new \InvalidArgumentException('Parametro time debe ser DateTime o timestamp');
+			}
+
+			$eta = $this->getDateTimeObj((int) $time);
+		}
+
+		$this->fecha_zarpe = $eta;
+
+		$d = (new \DateTime('@' . time()))->setTimezone(new \DateTimeZone('America/Mexico_City'));
+
+		if ( $eta->getTimestamp() > $d->getTimestamp() ) {
+			$this->status = 'ETD';
+		}
+
+		return $this;
+	}
 }
--- src/Operations/Import.php
+++ src/Operations/Import.php
@@ -7,7 +7,7 @@ namespace Operations;
  */
 class Import extends Operation
 {
-	protected $fecha_origen, $pais_origen;
+	protected $fecha_arribo, $pais_origen;
 	
 	function __construct(
 		$time, string $pais, string $tipo_mercancia, array $mercancias, string $status='Alta'
@@ -23,7 +23,28 @@ class Import extends Operation
 			$time = $this->getDateTimeObj((int) $time);
 		}
 
-		$this->fecha_origen = $time;
+		$this->fecha_arribo = $time;
 		$this->pais_origen = $pais;
+	}
+
+	public function actualizarFechaArribo($time) :self
+	{
+		if ( !$time instanceof \DateTime ) {
+			if ( empty((int) $time) ) {
+				throw new \InvalidArgumentException('Parametro time debe ser DateTime o timestamp');
+			}
+
+			$eta = $this->getDateTimeObj((int) $time);
+		}
+
+		$this->fecha_arribo = $eta;
+
+		$d = (new \DateTime('@' . time()))->setTimezone(new \DateTimeZone('America/Mexico_City'));
+
+		if ( $eta->getTimestamp() > $d->getTimestamp() ) {
+			$this->status = 'ETA en Espera';
+		}
+
+		return $this;
 	}
 }
